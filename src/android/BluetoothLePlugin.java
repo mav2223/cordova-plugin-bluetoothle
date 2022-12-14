79fb3943ad7d3c7ca622050343e9fdeb816bb589
mav2223 <mavis.black2223@gmail.com>
2022-12-14 10:43:46 +0800

    #663 fix

--- src/android/BluetoothLePlugin.java
+++ src/android/BluetoothLePlugin.java
@@ -2755,12 +2755,12 @@ public class BluetoothLePlugin extends CordovaPlugin {
       return;
     }
     
-    String address = getAddress(obj);
+    final String address = getAddress(obj);
     if (isNotAddress(address, callbackContext)) {
       return;
     }
 
-    String pin = getPin(obj);
+    final String pin = getPin(obj);
     if (pin==null) {
       return;
     }
